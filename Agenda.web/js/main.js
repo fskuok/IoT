aed42c10f37e33c716e3007ca41144d18c192113
Fan Sai Kuok <fskuok@yahoo.com>
2014-11-15 23:25:43 -0500

    Added panel
    Added panel
    Organized services and controllers of angular app

--- Agenda.web/js/main.js
+++ Agenda.web/js/main.js
@@ -1,137 +1,237 @@
 // link of the data sheet
 var AGENDA_GOOGLE_SPREADSHEET_KEY = "1rGsUNIk_zhfuNUMglR-4My7vPqku-sZg8JBYkKp9Qf4",
-	ACCESS_TOKEN = "6564c0732ab5d528aee3a4b84b9f35b77604d026",
+    SPARK_SPREADSHEET_KEY =         "11fi7mpyJZNgMvyP9qtCYFzPUNSQV4CDGsyXFqZnOl10",
 	DEVICES = {
 			"DOOR": "53ff6e066667574826422167"
 		};
 
-function RETURN_URL(DEVICE_KEY, VARIABLE_NAME){
 
-	return  "https://api.spark.io/v1/devices/" +
-					 DEVICE_KEY + "/" +  VARIABLE_NAME + "?access_token=" + ACCESS_TOKEN;
-
-};
-
-
-;(function(){
+(function(){
 
     'use strict';
 
-	var agendaApp = angular.module('agendaApp', []);
+	var agendaApp = angular.module('agendaApp', [])
+
+        //using tabletop.js https://github.com/jsoma/tabletop
+        //to fetch data form google spreadsheet
+        .factory('tableTop', function(){
+            function get(key, callback) {
+                var param = {
+                    key: key,
+                    simpleSheet: true,
+                    callback: callback
+                };
+
+                Tabletop.init( param );
+            }
+
+            return {
+                get: get
+            }
+        })
+
+        //services that connect spark boards
+        .factory('spark', ['$http', function($http){
+            function RETURN_URL(DEVICE_KEY, VARIABLE_NAME){
+
+                return  "https://api.spark.io/v1/devices/" + DEVICE_KEY + "/" +  VARIABLE_NAME + "?access_token=6564c0732ab5d528aee3a4b84b9f35b77604d026";
+
+            }
+
+            function getVar(deviceID, varName, onSuccess){
+                $http
+                    .get( RETURN_URL(deviceID, varName) )
+                    .success( onSuccess );
+            }
+
+            function triggerFn(deviceID, funcName, args, onSuccess) {
+                $http
+                    .post( RETURN_URL(deviceID, funcName), {"args": args} )
+                    .success( onSuccess );
+            }
+
+            return {
+                getVar: getVar,
+                triggerFn: triggerFn
+            }
+        }])
+
+        .factory('dom', function(){
+            var panel =  document.querySelector('.panel');
+
+            panel.on = function(){
+                this.style.display = 'block';
+                return this;
+            };
+
+            panel.off = function(){
+                this.style.display = 'none';
+            };
+
+            function scrollTo(n) {
+                n = n || 1;
+                $('div.event').css('top', 200 - 130*n);
+            }
+
+            function nextEvent(n) {
+                n = n || 1;
+                $('div.event').css('top', '-=' + 130*n);
+            }
+
+            function prevEvent(n) {
+                n = n || 1;
+                $('div.event').css('top', '+=' + 130*n);
+            }
+
+            return {
+                events:{
+                    scrollTo: scrollTo,
+                    nextEvent: nextEvent,
+                    prevEvent: prevEvent
+                },
+                panel: panel
+            }
+        })
+
+        .factory('time', function(){
+
+            //Calculate the interval of start and end
+            //@param: String: 'xx:xx' | Date object, String: 'xx:xx' | Date object
+            //@return: Strings
+
+            function duration(start, end){
+
+                var sarr = typeof start === 'string' ?
+                        start.split(':') : [start.getHours(), start.getMinutes()],
+                    earr = typeof start === 'string' ?
+                        end.split(':') : [end.getHours(), end.getMinutes()],
+                    minsTotal = (earr[0] - sarr[0])*60 + (earr[1] - sarr[1]);
+
+                return [Math.floor( minsTotal/60 ), ( minsTotal%60 )]
+            }
+
+            return {
+                duration: duration
+            }
+        })
 
 
-	agendaApp.controller('asideCtrl', ['$scope', function($scope){
+        .controller('agendaCtrl',
 
-		//set date data
-		$scope.date = function(){
-			var arr = new Date().toString().split(' ')
-			return arr[0] + ' | ' + arr[1] + ' ' + arr[2];
-		};
+            ['$scope', '$http', 'tableTop', 'dom', 'time',
+                function( $scope, $http, tableTop, dom, time){
+                    $scope.panelMessage = 'Loading...';
+                    $scope.nowEvent = {};
 
-		$scope.time = {};
+                    //fetch agenda data
+                    tableTop.get( AGENDA_GOOGLE_SPREADSHEET_KEY, function(data) {
+                        $scope.$apply( function() {
+                            $scope.events = data;
+                            if( $scope.isOn( data[0].start, data[ data.length - 1 ].end ) === 'now'){
+                                dom.panel.off();
+                            }else{
+                                $scope.panelMessage = 'Meeting ended';
+                            }
 
-		//Refresh the time for the clock
-		function refreshTime(){
-			var time = new Date();
-
-			$scope.time.hour = 
-					time.getHours() < 10 ? '0' + time.getHours(): time.getHours();
-			$scope.time.min = 
-					time.getMinutes() < 10 ? '0' + time.getMinutes(): time.getMinutes();
-			$scope.time.sec = 
-					time.getSeconds() < 10 ? '0' + time.getSeconds(): time.getSeconds();
-			
-		}
-
-		//
-		//$scope.$apply(refreshTime);
-
-		//
-		setInterval(function(){
-			$scope.$apply(refreshTime);
-		}, 1000);
-
-	}]);
-
-	agendaApp.controller('agendaCtrl', 
-
-		['$scope', '$rootScope', '$http', function($scope, $rootScope, $http){
-
-		//Calculate the interval of start and end
-		//@param: String: 'xx:xx', String: 'xx:xx'
-		//@return: Strings
-		$scope.calulateDuration = function(start, end){
-			var sarr = start.split(':'),
-				earr = end.split(':'),
-				minsTotal = (earr[0] - sarr[0])*60 + (earr[1] - sarr[1]);
-			return Math.floor(minsTotal/60) + 'H' + (minsTotal%60) + 'M'
-		};
-
-		//check if now time is between a time period
-		//@return: Boolean
-		$scope.isOn = function(start, end, eventId){
-			var s = new Date(), e = new Date(), n = new Date();
-
-			s.setHours( start.split(':')[0] );
-			s.setMinutes( start.split(':')[1] );
-			s.setSeconds(0);
-			e.setHours( end.split(':')[0] );
-			e.setMinutes( end.split(':')[1] );
-			e.setSeconds(0);
-
-			
+                        });
+                    });
 
 
-			//now hour is between start hour and end hour
-			return ( s < n && n < e ) ? 
-							(goTo(eventId), 'now')
-							: undefined;
-		}
 
-		//using tabletop.js https://github.com/jsoma/tabletop 
-		//to fetch data form google spreadsheet
-		$scope.getDataFromGoogleSS = function() {
-			var param = { 
-							key: AGENDA_GOOGLE_SPREADSHEET_KEY,
-							callback: function(data) { 
-								$scope.$apply( function() {
-									$scope.events = data;
-								} );
-							},
-							simpleSheet: true 
-						};
 
-			Tabletop.init( param );
-		};
-		
-		$scope.getDataFromGoogleSS();
 
-	}]);
+                    //set date data
+                    $scope.date = function(){
+                        var arr = new Date().toString().split(' ');
+                        return arr[0] + ' | ' + arr[1] + ' ' + arr[2];
+                    };
+
+                    //
+                    $scope.duration = time.duration;
+
+                    $scope.time = {};
+
+                    //Refresh the time for the clock
+                    function refreshTime(){
+                        var nowTime = new Date(),
+                            remainingTime;
+
+                        $scope.time.hour =
+                                nowTime.getHours() < 10 ? '0' + nowTime.getHours(): nowTime.getHours();
+
+                        $scope.time.min =
+                                nowTime.getMinutes() < 10 ? '0' + nowTime.getMinutes(): nowTime.getMinutes();
+
+                        $scope.time.sec =
+                                nowTime.getSeconds() < 10 ? '0' + nowTime.getSeconds(): nowTime.getSeconds();
+
+                        if($scope.nowEvent.endDate){
+                            remainingTime = time.duration(nowTime, $scope.nowEvent.endDate);
+                            $scope.remainingHours = remainingTime[0];
+                            $scope.remainingMins = remainingTime[1];
+                        }
+
+                    }
+                    refreshTime();
+
+                    //check if now time is between a time period
+                    //@return: Boolean
+                    $scope.isOn = function(start, end, eventId){
+
+                        var s = new Date(), e = new Date(), n = new Date();
+
+                        start = start.split(':');
+                        end = end.split(':');
+
+                        s.setHours( start[0] );
+                        s.setMinutes( start[1] );
+                        s.setSeconds(0);
+                        e.setHours( end[0] );
+                        e.setMinutes( end[1] );
+                        e.setSeconds(0);
+
+                        if( s <= n && n < e ){
+                            if(eventId){
+                                dom.events.scrollTo( eventId );
+
+                                $scope.nowEvent.id = eventId;
+                                $scope.nowEvent.startDate = s;
+                                $scope.nowEvent.endDate = e;
+
+                            }
+                            return 'now';
+
+                        }else if( n >= e ){
+
+                            return 'before';
+                        }else{
+
+                            return 'after';
+                        }
+                    };
+
+
+
+
+
+                    //refreshed by seconds
+                    setInterval(function(){
+                        $scope.$apply(refreshTime);
+                    }, 1000);
+
+
+
+
+            }]
+        );
+
+    //agendaApp ends
+
+
+
 })();
 
-function goTo(n) {
-	n = n || 1;
-	$('div.event').css('top', 200 - 130*n);
-}
 
-function nextEvent(n) {
-	n = n || 1;
-	$('div.event').css('top', '-=' + 130*n);
-}
 
-function prevEvent(n) {
-	n = n || 1;
-	$('div.event').css('top', '+=' + 130*n);
-}
 
-function getVar(deviceID, varName, onSuccess){
-			$http
-			.get(RETURN_URL(deviceID, varName))
-			.success(onSuccess);
-		}
 
-function triggerFn(deviceID, funcName, args, onSuccess) {
-	$http
-		.post(RETURN_URL(DEVICES.DOOR, funcName), {"args": args})
-		.success(onSuccess);
-}
